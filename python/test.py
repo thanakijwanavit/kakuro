9fdbd840425c97c37dd34a5c950523145ccd13fe
Nic <Wanavit>
2018-06-09 14:06:46 +0700

    add api

--- python/test.py
+++ python/test.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-# enable debugging
 import cgitb
+
 cgitb.enable()
+r = requests.get('https://tfngbfhvqd.execute-api.us-east-1.amazonaws.com/deploymenttest/kakuro?toplength=3&topsum=7&sidelength=3&sidesum=10')
 print("Content-Type: text/html;charset=utf-8")
-print()
-print("Hello World!")
+print(r)
+
