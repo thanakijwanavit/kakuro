3f11fbf9f82839d01264837001b59ed766e2f81f
Nic <Wanavit>
2018-06-09 14:10:32 +0700

    import requests

--- python/test.py
+++ python/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-# enable debugging
 import cgitb
+import requests
 
 cgitb.enable()
 r = requests.get('https://tfngbfhvqd.execute-api.us-east-1.amazonaws.com/deploymenttest/kakuro?toplength=3&topsum=7&sidelength=3&sidesum=10')
