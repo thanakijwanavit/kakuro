7a301d843ff49c7fa70912bcc2ec8f1b9658bfd0
Nic <Wanavit>
2018-06-09 14:12:59 +0700

    requests get library

--- python/test.py
+++ python/test.py
@@ -6,5 +6,5 @@ import requests
 cgitb.enable()
 r = requests.get('https://tfngbfhvqd.execute-api.us-east-1.amazonaws.com/deploymenttest/kakuro?toplength=3&topsum=7&sidelength=3&sidesum=10')
 print("Content-Type: text/html;charset=utf-8")
-print(r)
+print(str(r))
 
