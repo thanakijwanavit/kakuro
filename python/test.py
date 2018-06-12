8a2b4fa7dd3473fd80f9863249e922cc927bc676
Nic <Wanavit>
2018-06-12 08:22:17 +0700

    remove a line

--- python/test.py
+++ python/test.py
@@ -6,6 +6,6 @@ import requests
 cgitb.enable()
 r = requests.get('https://tfngbfhvqd.execute-api.us-east-1.amazonaws.com/deploymenttest/kakuro?toplength=3&topsum=7&sidelength=3&sidesum=10')
 
-print("Content-Type: Content-type: text/html\n\n")
+print("Content-Type: Content-type: text/html\n")
 print(str(r.text))
 #print ("hello world")
