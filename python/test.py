917a5a3eb825762141e969114c01ca488f0c40c2
Nic <Wanavit>
2018-06-12 08:12:12 +0700

    test hello world

--- python/test.py
+++ python/test.py
@@ -4,7 +4,8 @@ import cgitb
 import requests
 
 cgitb.enable()
-r = requests.get('https://tfngbfhvqd.execute-api.us-east-1.amazonaws.com/deploymenttest/kakuro?toplength=3&topsum=7&sidelength=3&sidesum=10')
-print("Content-Type: text/html;charset=utf-8")
-print(str(r.text))
+#r = requests.get('https://tfngbfhvqd.execute-api.us-east-1.amazonaws.com/deploymenttest/kakuro?toplength=3&topsum=7&sidelength=3&sidesum=10')
 
+print("Content-Type: text/html;charset=utf-8")
+#print(str(r.text))
+print ("hello world")
