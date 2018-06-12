9652f2e824f5784732faa85db58ca69507b6d3fa
Nic <Wanavit>
2018-06-12 08:21:06 +0700

    test with the actual api

--- python/test.py
+++ python/test.py
@@ -4,8 +4,8 @@ import cgitb
 import requests
 
 cgitb.enable()
-#r = requests.get('https://tfngbfhvqd.execute-api.us-east-1.amazonaws.com/deploymenttest/kakuro?toplength=3&topsum=7&sidelength=3&sidesum=10')
+r = requests.get('https://tfngbfhvqd.execute-api.us-east-1.amazonaws.com/deploymenttest/kakuro?toplength=3&topsum=7&sidelength=3&sidesum=10')
 
 print("Content-Type: Content-type: text/html\n\n")
-#print(str(r.text))
-print ("hello world")
+print(str(r.text))
+#print ("hello world")
